596261fb86fe46535944205e3b0499d393377be6
Defanda Yeremia <144663115+doctordoom101@users.noreply.github.com>
2026-01-04 23:45:33 +0700

    ADD: automate data processing

---
+++ preprocessing/automate_defanda-yeremia.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+RAW_DATA_PATH = os.path.join(BASE_DIR, '../fraud_detection_raw/card_transdata.csv')
+OUTPUT_DIR = os.path.join(BASE_DIR, 'fraud_detection_preprocessing')
+OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'clean_card_transdata.csv')
+
+def process_data():
+    print("Memulai proses data cleaning...")
+    
+    # 2. Load Data
+    df = pd.read_csv(RAW_DATA_PATH)
+    
+    # 3. Preprocessing 
+    df.dropna(inplace=True)
+    
+    print(f"Data diproses. Ukuran akhir: {df.shape}")
+    
+    # 4. Simpan Data
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    df.to_csv(OUTPUT_FILE, index=False)
+    print(f"Data berhasil disimpan di: {OUTPUT_FILE}")
+
+if __name__ == "__main__":
+    process_data()
