552b5460e2bcd2a07b5b2c819b55e27f073e58bb
Defanda Yeremia <144663115+doctordoom101@users.noreply.github.com>
2026-01-08 23:59:42 +0700

    UPDATE preprocessing

--- preprocessing/automate_defanda-yeremia.py
+++ preprocessing/automate_defanda-yeremia.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import os
+import pandas as pd
+from sklearn.preprocessing import RobustScaler
+import joblib
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 RAW_DATA_PATH = os.path.join(BASE_DIR, '../credit_card_fraud_data_raw/card_transdata.csv')
@@ -8,19 +10,46 @@ OUTPUT_FILE = os.path.join(OUTPUT_DIR, 'clean_card_transdata.csv')
 
 def process_data():
     print("Memulai proses data cleaning...")
-    
-    # 2. Load Data
+
+    # 1. Load Data
     df = pd.read_csv(RAW_DATA_PATH)
-    
-    # 3. Preprocessing 
-    df.dropna(inplace=True)
-    
-    print(f"Data diproses. Ukuran akhir: {df.shape}")
-    
-    # 4. Simpan Data
+
+    print(f"Ukuran awal data: {df.shape}")
+
+    # 2. Drop missing value
+    df = df.dropna()
+    print(f"Setelah drop NA: {df.shape}")
+
+    # 3. Fix binary columns dtype
+    binary_cols = [
+        'repeat_retailer',
+        'used_chip',
+        'used_pin_number',
+        'online_order',
+        'fraud'
+    ]
+
+    df[binary_cols] = df[binary_cols].astype(int)
+
+    # 4. Scaling numeric features (ROBUST)
+    num_cols = [
+        'distance_from_home',
+        'distance_from_last_transaction',
+        'ratio_to_median_purchase_price'
+    ]
+
+    scaler = RobustScaler()
+    df[num_cols] = scaler.fit_transform(df[num_cols])
+
+    # 5. Save processed data & scaler
     os.makedirs(OUTPUT_DIR, exist_ok=True)
+
     df.to_csv(OUTPUT_FILE, index=False)
-    print(f"Data berhasil disimpan di: {OUTPUT_FILE}")
+    joblib.dump(scaler, os.path.join(OUTPUT_DIR, "robust_scaler.joblib"))
+
+    print(f"Data berhasil diproses.")
+    print(f"- Data final: {OUTPUT_FILE}")
+    print(f"- Scaler disimpan: robust_scaler.joblib")
 
 if __name__ == "__main__":
     process_data()
